6aad10c567b6fbad5dc53bc57da1cc4efa7e2f32
Utkarsh <68469904+utkarsh11252@users.noreply.github.com>
2022-07-24 06:04:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import streamlit as st
 def main():
     st.title("""Multiplication of 2 numbers""")
 
-    one=st.number_input("First_Number")
-    two=st.number_input("Second number")
+    one=st.number_input("Enter first_Number")
+    two=st.number_input("Enter second number")
 
     ans=one*two
 
